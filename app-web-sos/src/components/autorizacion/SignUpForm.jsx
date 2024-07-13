f42e06ddfdbdca8acff849f216a95dcff6ff99d1
Spoxbz <minerolgestbz@gmail.com>
2024-07-13 14:41:48 -0500

    Update SignUpForm.jsx

--- app-web-sos/src/components/autorizacion/SignUpForm.jsx
+++ app-web-sos/src/components/autorizacion/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { supabase } from '../../api/supabase';
 import { Box, Button, FormControl, FormLabel, Input, Select } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
