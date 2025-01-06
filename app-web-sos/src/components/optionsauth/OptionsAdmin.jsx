8484f528472dd5a3165516e00ad3cb0462736e5f
Nathaly <soscuencaecuador@gmail.com>
2025-01-06 18:44:30 -0500

    cambios de iconos

--- app-web-sos/src/components/optionsauth/OptionsAdmin.jsx
+++ app-web-sos/src/components/optionsauth/OptionsAdmin.jsx
@@ -1,76 +1,91 @@
-//import React from 'react';
-import { Button, Box, SimpleGrid, Text, Icon } from '@chakra-ui/react';
-import { FaClipboard } from 'react-icons/fa';
+import React from 'react';
+import { Button, Box, SimpleGrid, Text, Image } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 
+
+import registrarPacienteIcon from "../../assets/registrarPacientes.svg";
+import consultarCierredeCajaIcon from "../../assets/consultarCierredeCaja.svg"
+import cierredeCajaIcon from "../../assets/cierredeCaja.svg"
+import laboratorioOrdenIcon from "../../assets/laboratorioOrden.svg"
+import enviosIcon from "../../assets/envios.svg"
+import ventaIcon from "../../assets/venta.svg"
+import entregasIcon from "../../assets/entregas.svg"
+import saldosIcon from "../../assets/saldos.svg"
+import egresosIcon from "../../assets/egresos.svg"
+import historiaClinicaIcon from "../../assets/historiaClinica.svg"
+import garantiaIcon from "../../assets/garantia.svg"
+import inventarioIcon from "../../assets/inventario.svg"
+import usuariosIcon from "../../assets/usuarios.svg"
+import laboratoriosIcon from "../../assets/laboratorios.svg"
+import sucursalesIcon from "../../assets/sucursales.svg"
+import certificadoVisualIcon from "../../assets/certificadoVisual.svg"
+import medidasIcon from "../../assets/medidas.svg"
+
+
+
+
 const options = [
-  { label: "REGISTRAR PACIENTE" },
-  { label: "HISTORIAL PACIENTE" },
-  { label: "ORDEN DE LABORATORIO" },
-  { label: "ENVIOS" },
-  { label: "VENTA/ CONTRATO DE SERVICIO" },
-  { label: "RETIROS" },
-  { label: "CIERRE" },
-  { label: "SALDOS" },
-  { label: "EGRESOS" },
-  { label: "GARANTIA" },
-  { label: "CONSUL GARANTIA" },
-  { label: "INVENTARIO" },
-  { label: "USUARIOS" },
-  { label: "LABORATORIOS" },
-  { label: "SUCURSAL" },
-  { label: "CONSULTAR CIERRE" },
-  { label: "IMPRIMIR CERTIFICADO" },
-  { label: "REGISTRAR LUNAS" },
-  {label: "REGISTRAR MEDIDAS"}
+  { label: "REGISTRAR PACIENTE", icon: registrarPacienteIcon },
+  { label: "HISTORIAL PACIENTE", icon:  historiaClinicaIcon }, 
+  { label: "ORDEN DE LABORATORIO", icon: laboratorioOrdenIcon }, 
+  { label: "ENVIOS", icon: enviosIcon }, 
+  { label: "VENTA/ CONTRATO DE SERVICIO", icon: ventaIcon },
+  { label: "RETIROS", icon: entregasIcon },
+  { label: "CIERRE", icon: cierredeCajaIcon },
+  { label: "SALDOS", icon: saldosIcon },
+  { label: "EGRESOS", icon: egresosIcon },
+  { label: "GARANTIA", icon: garantiaIcon },
+  { label: "CONSUL GARANTIA", icon: registrarPacienteIcon },
+  { label: "INVENTARIO", icon: inventarioIcon },
+  { label: "USUARIOS", icon: usuariosIcon },
+  { label: "LABORATORIOS", icon: laboratoriosIcon },
+  { label: "SUCURSAL", icon: sucursalesIcon },
+  { label: "CONSULTAR CIERRE", icon: consultarCierredeCajaIcon },
+  { label: "IMPRIMIR CERTIFICADO", icon: certificadoVisualIcon },
+  { label: "REGISTRAR LUNAS", icon: registrarPacienteIcon },
+  { label: "REGISTRAR MEDIDAS", icon: medidasIcon }
 ];
 
 const AdminDashBoard = () => {
   const navigate = useNavigate();
 
   const handleOptionClick = (label) => {
-    if (label === "USUARIOS") {
-      navigate('/Register');
-    }
-    // añadir más condiciones para otras opciones si es necesario
-    if (label === "REGISTRAR PACIENTE") {
-      navigate('/RegisterPatient');
-    }
-
-    if (label === "INVENTARIO") {
-      navigate('/Inventory');
-    }
-
-    if (label === 'SUCURSAL'){
-      navigate('/Branch')
-    }
-
-    if (label === 'LABORATORIOS'){
-      navigate('/Labs')
-    }
-
-    if (label === "CONSULTAR CIERRE"){
-      navigate('/CashClousure')
-    }
-
-    if (label === 'VENTA/ CONTRATO DE SERVICIO') {
-      navigate('/SalesForm')
-    }
-
-    if (label === "REGISTRAR LUNAS") {
-      navigate('/RegisterLens')
-    }
-
-    if (label === "REGISTRAR MEDIDAS") {
-      navigate('/RegisterMeasures')
-    }
-
-    if (label === "CIERRE") {
-      navigate('/PatientRecords')
-    }
-
-    if (label === "EGRESOS") {
-      navigate ('/Egresos')
+    switch (label) {
+      case "USUARIOS":
+        navigate('/Register');
+        break;
+      case "REGISTRAR PACIENTE":
+        navigate('/RegisterPatient');
+        break;
+      case "INVENTARIO":
+        navigate('/Inventory');
+        break;
+      case "SUCURSAL":
+        navigate('/Branch');
+        break;
+      case "LABORATORIOS":
+        navigate('/Labs');
+        break;
+      case "CONSULTAR CIERRE":
+        navigate('/CashClousure');
+        break;
+      case "VENTA/ CONTRATO DE SERVICIO":
+        navigate('/SalesForm');
+        break;
+      case "REGISTRAR LUNAS":
+        navigate('/RegisterLens');
+        break;
+      case "REGISTRAR MEDIDAS":
+        navigate('/RegisterMeasures');
+        break;
+      case "CIERRE":
+        navigate('/PatientRecords');
+        break;
+      case "EGRESOS":
+        navigate('/Egresos');
+        break;
+      default:
+        break;
     }
   };
 
@@ -80,25 +95,31 @@ const AdminDashBoard = () => {
 
   return (
     <>
-    <Button onClick={() => handleNavigate('/Login')} mt={4}>
-          Cerrar Sesión
-        </Button>
+      <Button onClick={() => handleNavigate('/Login')} mt={4}>
+        Cerrar Sesión
+      </Button>
       <SimpleGrid columns={[2, null, 4]} spacing={5}>
-      {options.map((option, index) => (
-        <Box
-          key={index}
-          textAlign="center"
-          p={5}
-          boxShadow="md"
-          borderRadius="md"
-          _hover={{ bg: "gray.100", cursor: "pointer" }}
-          onClick={() => handleOptionClick(option.label)}
-        >
-          <Icon as={FaClipboard} boxSize={10} mb={3} />
-          <Text>{option.label}</Text>
-        </Box>
-      ))}
-    </SimpleGrid>
+        {options.map((option, index) => (
+          <Box
+            key={index}
+            textAlign="center"
+            p={5}
+            boxShadow="md"
+            borderRadius="md"
+            _hover={{ bg: "gray.100", cursor: "pointer" }}
+            onClick={() => handleOptionClick(option.label)}
+          >
+            <Image
+              src={option.icon}
+              alt={option.label}
+              boxSize="40px"
+              mb={3}
+              mx="auto"
+            />
+            <Text>{option.label}</Text>
+          </Box>
+        ))}
+      </SimpleGrid>
     </>
   );
 };
