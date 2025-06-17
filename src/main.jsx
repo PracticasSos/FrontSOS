09a61528b5fa8cc75aaafdfb0cd90dc25b661df9
Freddy Paguay <freddypaguay20@gmail.com>
2025-06-17 15:33:36 -0500

    Cambio de fuente global

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './Styles/global.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import theme from './theme'; // Importa el theme personalizado
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </ChakraProvider>
-)
+);
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+// src/theme.js
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Satoshi', sans-serif`,
+    body: `'Satoshi', sans-serif`,
+  },
+});
+
+export default theme;
